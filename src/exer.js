73d0e8595317648ba772119208a93aa8b4f1c4d1
Nichoas Ito <nicholas.ito94@gmail.com>
2023-07-26 14:48:34 -0300

    Adicionar solução do exercício ES6+

---
+++ src/exer.js
@@ -0,0 +1,15 @@
+// array de objetos com nome e nota dos alunos
+const alunos = [
+    { nome: 'Gustavo', nota: 7 },
+    { nome: 'Julia', nota: 5 },
+    { nome: 'Paula', nota: 8 },
+    { nome: 'Wagner', nota: 6 },
+];
+
+// função q retorna apenas os alunos com nota maior ou igual a 6
+const alunosAprovados = alunos.filter((aluno) => aluno.nota >= 6);
+
+// console.log(alunosAprovados);
+alunosAprovados.forEach(({ nome, nota }) => {
+    console.log(`Aluno(a):${nome}, está aprovado com nota: ${nota}`);
+});
